cec4d9f397fb8b53905559e05ac0913ff14b0db4
atacanjacques <atacan.jacques@gmail.com>
2017-11-02 08:53:03 +0100

    CRUD

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -17,17 +17,37 @@ class PostController extends Controller
   /**
   * @Route("/", name="post_index")
   * @Method({"GET"})
+  * @return Response
   */
   public function indexAction()
   {
-    return $this->render('post/index.html.twig');
+    $em = $this->getDoctrine()->getManager();
+    $posts = $em->getRepository('AppBundle:Post')->findBy(['removed' => false]);
+    return $this->render('post/index.html.twig', [
+      'posts' => $posts
+    ]);
   }
 
   /**
-  * @Route("/create", name="post_create")
-  * @Method({"GET", "POST"})
+  * @Route("/{slug}/show", name="post_show")
+  * @Method({"GET"})
+  * @param Article $article
+  * @return Response
   */
-  public function createAction(Request $request)
+  public function showAction()
+  {
+    return $this->render('post/show.html.twig', [
+      'post' => $post
+    ]);
+  }
+
+  /**
+  * @Route("/new", name="post_new")
+  * @Method({"GET", "POST"})
+  * @param Request $request
+  * @return RedirectResponse|Response
+  */
+  public function newAction(Request $request)
   {
     $post = new Post();
     $form = $this->createForm(PostType::class, $post);
@@ -44,9 +64,59 @@ class PostController extends Controller
       return $this->redirectToRoute('post_index');
     }
 
-    return $this->render('post/create.html.twig',[
+    return $this->render('post/new.html.twig',[
       'post' => $post,
       'form' => $form->createView()
     ]);
+  }
+
+  /**
+  * @Route("/update", name="post_new")
+  * @Method({"GET", "POST"})
+  * @param Request $request
+  * @return RedirectResponse|Response
+  */
+  public function updateAction(Request $request, Post $post)
+  {
+    $form = $this->createForm(PostType::class, $post);
+
+    $form->handleRequest($request);
+    if($form->isValid())
+    {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($post);
+      $em->flush();
+
+      $this->addFlash('succes', "L'article {$post->getTitle()} à été modifié");
+
+      return $this->redirectToRoute('post_index', ['slug'] => $post);
+    }
+
+    return $this->render('post/new.html.twig',[
+      'post' => $post,
+      'form' => $form->createView()
+    ]);
+  }
+
+  /**
+  * @Route("/delete/{id}/{token}", name="post_delete")
+  * @Method({"GET"})
+  * @param Request $request
+  * @param Article $article
+  * @return RedirectResponse|Response
+  */
+  public function deleteAction(Request $request, Post $post)
+  {
+    $token = $request->attributes->get('token');
+
+    if(!$this->isCsrfTokenValid('delete_article', $token));
+
+    $article->setRemoved(true);
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($post);
+    $em->flush();
+
+    return $this->redirectToRoute('post_index');
   }
 }
